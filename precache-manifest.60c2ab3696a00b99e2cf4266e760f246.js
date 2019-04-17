880464c6c1cb54f29f6f79f844c705b08d738ce0
Promit Dutta <promit.dutta@accenture.com>
2019-04-17 19:25:05 +0530

    Updates

---
+++ precache-manifest.60c2ab3696a00b99e2cf4266e760f246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "01683e99765fc174cfdc",
+    "url": "/react-github-pages/static/js/runtime~main.417fbdf7.js"
+  },
+  {
+    "revision": "12732738feb7cc95bb8e",
+    "url": "/react-github-pages/static/js/main.42ec63c1.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-github-pages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "12732738feb7cc95bb8e",
+    "url": "/react-github-pages/static/css/main.5be92200.chunk.css"
+  },
+  {
+    "revision": "6ccd75c3b2b8c87fcf126af468296fc9",
+    "url": "/react-github-pages/index.html"
+  }
+];
